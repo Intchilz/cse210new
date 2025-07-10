80926adc251dc0d6ee0151618c12fb40ef300a00
Intchilz <chi21012@byui.edu>
2025-07-10 21:24:23 +0200

    debugged code

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -15,11 +15,9 @@ public class Journal
 
         //In this function, I save the file in my working directory
         // and give it the name that the user types.
-        string directoryPath = @"C:\Users\lchil\OneDrive\Desktop\cse210new";
         string path = _fileName;
-        string fullPath = Path.Combine(directoryPath, path);
 
-        using (FileStream fs = File.Create(fullPath))
+        using (FileStream fs = File.Create(path))
         {
             byte[] content = new System.Text.UTF8Encoding(true).GetBytes($"Date: {_date} - Prompt: {_question}\n {_FileContent}\n Entered at: {_time}\n \n");
             fs.Write(content, 0, content.Length);
@@ -29,41 +27,35 @@ public class Journal
 
     public void AppendToFile()
     {
-        string directoryPath = @"C:\Users\lchil\OneDrive\Desktop\cse210new";
         string FileName = _fileName;
 
-        string fullPath = Path.Combine(directoryPath, FileName);
         string newContent = $"Date: {_date} - Prompt: {_question}\n {_FileContent}\n Entered at: {_time}\n \n";
 
         // Append text to the file
-        File.AppendAllText(fullPath, newContent);
+        File.AppendAllText(FileName, newContent);
 
     }
 
     public void LoadFromFile()
     {
-        string path = @"C:\Users\lchil\OneDrive\Desktop\cse210new";
         string FileName = _fileName;
-        string fullPath = Path.Combine(path, FileName);
 
-        string content = File.ReadAllText(fullPath);
+        string content = File.ReadAllText(FileName);
         Console.WriteLine(content);
     }
 
     public void Read()
     {
-        string directoryPath = @"C:\Users\lchil\OneDrive\Desktop\cse210new";
         string path = _fileName;
-        string fullPath = Path.Combine(directoryPath, path);
 
-        string content = File.ReadAllText(fullPath);
+        string content = File.ReadAllText(path);
         Console.WriteLine(content);
     }
 
     public void createNewFile()
     {
-        string oldFileName = @"C:\Users\lchil\OneDrive\Desktop\cse210new\original.txt";
-        string newFileName = @"C:\Users\lchil\OneDrive\Desktop\cse210new\"+_fileName;
+        string oldFileName = "original.txt";
+        string newFileName = _fileName;
 
         File.Move(oldFileName, newFileName);
 
